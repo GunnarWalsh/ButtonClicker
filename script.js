efad76dccc602bd517d526f64ed8113fd2a7687f
GunnarWalsh <104541337+GunnarWalsh@users.noreply.github.com>
2022-08-22 21:19:40 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function displayLike(ninjaliked){
+    console.log(ninjaliked)
+}
+function logOut(element) {
+    element.innerText =
+"Log Out";
+}
+function hide(element) {
+    element.remove();
+}
+
